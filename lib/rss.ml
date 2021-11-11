ae786d7546d1a266a372447c78c385e3615b3f20
TheLocust3 <jake.kinsella@gmail.com>
2021-11-11 16:13:15 -0500

    Start modelling rss

--- lib/rss.ml
+++ lib/rss.ml
@@ -2,17 +2,28 @@ open Lwt
 open Cohttp_lwt_unix
 open Markup
 
+type xml = Content of string | Node of string * xml list
+type feed = {
+  title : string
+}
+
+let show { title } = "(Feed title = " ^ title ^ ")"
+
 let fromUri uri = 
   Client.get uri >>= fun (_, body) ->
     body |> Cohttp_lwt.Body.to_string >|= fun body -> body
 
-let fromXml body =
-  body |> parse_xml |> signals
+let fromBody body =
+  body |> parse_xml |> signals |> tree
+    ~text: (fun ss -> Content (String.concat "" ss))
+    ~element: (fun (_, name) _ children -> Node (name, children))
+
+let fromXml _ = Some { title = "test" }
 
 let feedFromUri uri = 
   fromUri uri >|= fun (body) ->
-    let feed = fromXml (string body) in
-    print_endline ("Parsed feed\n" ^ (feed |> pretty_print |> write_xml |> to_string));
+    let feed = body |> string |> fromBody |> fromXml in
+    print_endline (show (Option.get feed));
     ()
 
 let run () =
