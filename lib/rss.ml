cf4e63d75bd403aa683c3070434c583c11e6bd20
TheLocust3 <jake.kinsella@gmail.com>
2021-11-11 15:10:57 -0500

    Parse/output rss feed

--- lib/rss.ml
+++ lib/rss.ml
@@ -1,10 +1,20 @@
 open Lwt
 open Cohttp_lwt_unix
+open Markup
 
 let fromUri uri = 
   Client.get uri >>= fun (_, body) ->
     body |> Cohttp_lwt.Body.to_string >|= fun body -> body
 
+let fromXml body =
+  body |> parse_xml |> signals
+
+let feedFromUri uri = 
+  fromUri uri >|= fun (body) ->
+    let feed = fromXml (string body) in
+    print_endline ("Parsed feed\n" ^ (feed |> pretty_print |> write_xml |> to_string));
+    ()
+
 let run () =
-  let body = Lwt_main.run (fromUri (Uri.of_string "https://hnrss.org/frontpage")) in
-  print_endline ("Received body\n" ^ body)
+  let _ = Lwt_main.run (feedFromUri (Uri.of_string "https://hnrss.org/frontpage")) in
+  ()
