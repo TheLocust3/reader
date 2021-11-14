f5b5e2e6a8ca19115370ffdb720a2b6fea415571
TheLocust3 <jake.kinsella@gmail.com>
2021-11-13 22:04:12 -0500

    Parse rss feed

--- lib/rss.ml
+++ lib/rss.ml
@@ -3,12 +3,61 @@ open Cohttp_lwt_unix
 open Markup
 
 type xml = Content of string | Node of string * xml list
-type feed = {
-  title : string
-}
 
-let show { title } = "(Feed title = " ^ title ^ ")"
-let empty = { title = "" }
+module Item = struct
+  type item = {
+    title : string;
+    link : string;
+    description : string;
+  }
+
+  let show { title; link; description; } =
+    "  (Item
+         title = \"" ^ title ^ "\"
+         link = \"" ^ link ^ "\"
+         description = \"" ^ description ^ "\")"
+
+  let empty = { title = ""; link = ""; description = ""; }
+
+  let fromXml = function
+    Node ("item", children) ->
+      Some (List.fold_left(fun item child -> match child with
+       | Node ("title", [Content title]) -> { item with title = title }
+       | Node ("link", [Content link]) -> { item with link = link }
+       | Node ("description", [Content description]) -> { item with description = description }
+       | _ -> item
+    )(empty)(children))
+  | _ -> None
+end
+
+module Feed = struct
+  type feed = {
+    title : string;
+    link : string;
+    description : string;
+    items : Item.item list;
+  }
+
+  let show { title; link; description; items; } =
+    "(Feed
+       title = \"" ^ title ^ "\"
+       link = \"" ^ link ^ "\"
+       description = \"" ^ description ^ "\"
+       items = [\n" ^ String.concat "\n" (List.map Item.show items) ^ "])"
+
+  let empty = { title = ""; link = ""; description = ""; items = []; }
+
+  let fromXml = function
+    Node ("rss", [Node ("channel", children)]) ->
+     Some (List.fold_left(fun feed child -> match child with
+       | Node ("title", [Content title]) -> { feed with title = title }
+       | Node ("link", [Content link]) -> { feed with link = link }
+       | Node ("description", [Content description]) -> { feed with description = description }
+       | Node ("item", _) as node -> { feed with items = Option.get (Item.fromXml(node)) :: feed.items }
+       | _ -> feed
+    )(empty)(children))
+  | _ -> None
+end
 
 let fromUri uri = 
   Client.get uri >>= fun (_, body) ->
@@ -19,18 +68,10 @@ let fromBody body =
     ~text: (fun ss -> Content (String.concat "" ss))
     ~element: (fun (_, name) _ children -> Node (name, children))
 
-let fromXml = function
-  Node ("rss", [Node ("channel", children)]) ->
-   Some (List.fold_left(fun feed child -> match child with
-     | Node ("title", [Content title]) -> { title = title }
-     | _ -> feed
-   )(empty)(children))
-  | _ -> None
-
 let feedFromUri uri = 
   fromUri uri >|= fun (body) ->
-    let feed = body |> string |> fromBody |> Option.map(fromXml) |> Option.join in
-    print_endline (show (Option.get feed));
+    let feed = body |> string |> fromBody |> Option.map(Feed.fromXml) |> Option.join in
+    print_endline (Feed.show (Option.get feed));
     ()
 
 let run () =
