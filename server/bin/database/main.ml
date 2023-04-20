4ba30c83e296b72ec4052afe84c4309bec95d0ab
TheLocust3 <jake.kinsella@gmail.com>
2023-04-20 07:23:34 -0400

    Remove users

--- server/bin/database/main.ml
+++ server/bin/database/main.ml
@@ -1,17 +1,13 @@
 let migrate () =
   let connection = Lwt_main.run (Database.Connect.connect()) in
-  
-  let test_user = Model.User.Internal.build ~email: "jake.kinsella@gmail.com" ~password: (Option.value (Sys.getenv_opt "USER_PASSWORD") ~default: "foobar") in
 
   let _ = Lwt_main.run (Database.Feeds.migrate connection) in
   let _ = Lwt_main.run (Database.Items.migrate connection) in
-  let _ = Lwt_main.run (Database.Users.migrate connection) in
   let _ = Lwt_main.run (Database.Boards.migrate connection) in
   let _ = Lwt_main.run (Database.BoardEntries.migrate connection) in
   let _ = Lwt_main.run (Database.UserFeeds.migrate connection) in
   let _ = Lwt_main.run (Database.UserItems.migrate connection) in
 
-  let _ = Lwt_main.run (Database.Users.create test_user connection) in
     Printf.printf("Migration complete\n")
 
 let rollback () =
@@ -20,7 +16,6 @@ let rollback () =
   let _ = Lwt_main.run (Database.UserFeeds.rollback connection) in
   let _ = Lwt_main.run (Database.BoardEntries.rollback connection) in
   let _ = Lwt_main.run (Database.Boards.rollback connection) in
-  let _ = Lwt_main.run (Database.Users.rollback connection) in
   let _ = Lwt_main.run (Database.Items.rollback connection) in
   let _ = Lwt_main.run (Database.Feeds.rollback connection) in
     Printf.printf("Rollback complete\n")
