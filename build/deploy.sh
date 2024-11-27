d32d61bc3fbdcdbf03390ad814a2157591eeb753
TheLocust3 <jake.kinsella@gmail.com>
2024-11-27 09:39:41 -0500

    Refresh cluster deploy

--- build/deploy.sh
+++ build/deploy.sh
@@ -6,7 +6,6 @@ echo "Deploying to ${CONTROL_PLANE_IP} @ $VERSION"
 
 ./build/push.sh $VERSION
 
-ssh ubuntu@"${CONTROL_PLANE_IP}" "sudo ./ecr_refresh.sh"
-ssh ubuntu@"${CONTROL_PLANE_IP}" "sudo kubectl create secret generic reader-secrets --from-env-file secrets.env"
-ssh ubuntu@"${CONTROL_PLANE_IP}" "sudo kubectl create secret tls reader-cert --key=/etc/letsencrypt/live/jakekinsella.com/privkey.pem --cert=/etc/letsencrypt/live/jakekinsella.com/fullchain.pem"
+PASSWORD=$(ssh ubuntu@"${CONTROL_PLANE_IP}" "aws ecr get-login-password --region us-east-1")
+ssh -A ubuntu@"${CONTROL_PLANE_IP}" "sudo ./ecr_refresh.sh $PASSWORD"
 ssh ubuntu@"${CONTROL_PLANE_IP}" "sudo kubectl apply -f ~/cluster/reader/"
--- build/push.sh
+++ build/push.sh
@@ -9,6 +9,8 @@ export UI_IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/reade
 export IMAGE_POLICY="IfNotPresent"
 export HOST="reader.jakekinsella.com"
 export CENTRAL_BASE="http://central-server:8080/api"
+export NODE_SELECTOR="      nodeSelector:
+        node-role.kubernetes.io/control-plane: \"true\""
 
 rm -rf tmp/
 mkdir -p tmp/build/cluster/
@@ -17,5 +19,4 @@ for f in build/cluster/*.yaml; do envsubst < $f > tmp/$f; done
 
 ssh ubuntu@"${CONTROL_PLANE_IP}" "mkdir -p ~/cluster/reader/"
 
-scp -r secrets.env ubuntu@"${CONTROL_PLANE_IP}":~/secrets.env
 scp -r tmp/build/cluster/* ubuntu@"${CONTROL_PLANE_IP}":~/cluster/reader/
